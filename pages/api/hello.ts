5c86e09cabfbb017f0b9d452f2d5e0199db44110
Kiichi Tachibana <bodega1123@gmail.com>
2021-09-09 21:38:34 +0900

    Remove unused modules

--- pages/api/hello.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-const handler = (_: NextApiRequest, res: NextApiResponse) => {
-  const body = { message: 'Hello' }
-  res.statusCode = 200
-  res.json(body)
-}
-
-export default handler
